97734466458279e96cd7e1fa6264b3208b3a257b
Tom Tseng <tomwstseng@gmail.com>
2020-08-01 10:05:33 -0700

    hw

---
+++ Python/ForLoopBasic1/test.py
@@ -0,0 +1,35 @@
+
+# Basics
+for i in range(0,151):
+    print(i)
+
+# Multiples of Five
+for i in range(5,1001,5):
+    print(i)
+
+#Counting dojo way
+for i in range(1,101):
+    if i % 10 == 0:
+        print('Coding Dojo')
+    elif i % 5 == 0:
+        print('Coding')
+    else:
+        print(i)
+
+#Whoa. That Sucker's Huge
+sum = 0
+for i in range(1,500001,2):
+    sum += i
+print(sum)
+
+#Countdown by Fours
+for i in range(2018,0,-4):
+    print(i)
+
+#Flexible Counter
+lowNum = 2
+highNum = 9
+mult = 3
+for i in range(lowNum, highNum+1):
+    if i % mult == 0:
+        print(i)
