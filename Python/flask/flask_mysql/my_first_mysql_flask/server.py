24d5e04f9011e3e29b73b38ebad3f2810b9c957a
Tom Tseng <tomwstseng@gmail.com>
2020-08-05 08:27:00 -0700

    hw

---
+++ Python/flask/flask_mysql/my_first_mysql_flask/server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect, session
+from mysqlconnection import connectToMySQL
+
+app = Flask(__name__)
+app.secret_key = "myfoosecretkey"
+
+@app.route("/")
+def index():
+    mysql = connectToMySQL('first_flask')
+    friends = mysql.query_db('select * from friends')
+    print(friends)
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
