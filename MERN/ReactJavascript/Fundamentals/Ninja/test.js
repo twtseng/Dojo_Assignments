aa937c9bcc1f019b8a88e3ac405319b472feb506
twtseng <twtseng@users.noreply.github.com>
2020-08-29 14:07:49 -0700

    hw

--- MERN/ReactJavascript/Fundamentals/Ninja/test.js
+++ MERN/ReactJavascript/Fundamentals/Ninja/test.js
@@ -16,7 +16,27 @@ class Ninja {
     }
 }
 
+class Sensei extends Ninja {
+    constructor(name, health=200, speed=10, strength=10, wisdom=10) {
+        super(name, health);
+        this.speed = speed;
+        this.strength = strength;
+        this.wisdom = wisdom;
+    }
+    speakWisdom() {
+        super.drinkSake();
+        console.log("I just drank sake!");
+    }
+    showStats() {
+        console.log(`Ninja name: ${this.name}, speed: ${this.speed}, strength: ${this.strength} health: ${this.health} wisdom: ${this.wisdom}`);
+    }   
+}
 ninja = new Ninja('Joe', 123);
 ninja.showStats();
 ninja.drinkSake();
 ninja.showStats();
+
+sensei = new Sensei('Frank');
+sensei.showStats();
+sensei.speakWisdom();
+sensei.showStats();
