262f3901b20c6627874fa0cd6ad01d035d1e0044
twtseng <twtseng@users.noreply.github.com>
2020-08-29 11:21:53 -0700

    hw

---
+++ MERN/ReactJavascript/Fundamentals/Hoist/test.js
@@ -0,0 +1,91 @@
+// // 1
+// // undefined
+// console.log(hello);                                   
+// var hello = 'world';                                 
+// // 2
+// // magnet
+// var needle = 'haystack';
+// test();
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+// // 3 
+// // super cool
+// var brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+// // 4
+// // chicken
+// // half-chicken
+// var food = 'chicken';
+// console.log(food);
+// eat();
+// function eat(){
+//     food = 'half-chicken';
+//     console.log(food);
+//     var food = 'gone';
+//}
+// 5
+// mean is not a function
+// mean();
+// console.log(food);
+// var mean = function() {
+//     food = "chicken";
+//     console.log(food);
+//     var food = "fish";
+//     console.log(food);
+// }
+// console.log(food);
+
+// 6
+// rock
+// r&b
+// disco
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+
+// 7
+// san jose
+// seattle
+// burbank
+// san jose
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+//     dojo = "seattle";
+//     console.log(dojo);
+//     var dojo = "burbank";
+//     console.log(dojo);
+// }
+// console.log(dojo);
+
+// 8
+// {"name":"Chicago", "students":65, "hiring":true}
+// "closed for now"
+console.log(makeDojo("Chicago", 65));
+console.log(makeDojo("Berkeley", 0));
+function makeDojo(name, students){
+    const dojo = {};
+    dojo.name = name;
+    dojo.students = students;
+    if(dojo.students > 50){
+        dojo.hiring = true;
+    }
+    else if(dojo.students <= 0){
+        dojo = "closed for now";
+    }
+    return dojo;
+}
