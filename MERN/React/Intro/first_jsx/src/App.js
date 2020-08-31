ed0d642ec88000a40e3d71a9ab34f05cb229c138
twtseng <twtseng@users.noreply.github.com>
2020-08-31 13:40:17 -0700

    hw

---
+++ MERN/React/Intro/first_jsx/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb MT Rainier</li>
+        <li>Run a marathon</li>
+        <li>Feed the dog</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
