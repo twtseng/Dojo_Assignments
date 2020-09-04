b07eca5849232566e066180eadc2fe334ca328d3
twtseng <twtseng@users.noreply.github.com>
2020-09-04 15:18:40 -0700

    hw

--- MERN/React/Intro/form_validation/src/components/MyForm.js
+++ MERN/React/Intro/form_validation/src/components/MyForm.js
@@ -16,45 +16,38 @@ const initialState = {
 }
 
 function reducer(state, action) {
+    let error = null;
+    switch(action.type) {
+        case "first_name":
+            if (action.payload.length < 3) {
+                error = "First Name must be at least 3 chars";
+            }
+        break;
+        case "last_name":
+            if (action.payload.length < 4) {
+                error = "Last Name must be at least 4 chars";
+            }
+        break;    
+        case "email":
+            if (action.payload.length < 5) {
+                error = "email must be at least 5 chars";
+            }
+        break;
+        default:
+            break;
+    }
     return {
         ...state,
-        [action.type]: action.payload
+        [action.type]: { value: action.payload, error: error}
     };
 }
 const MyForm = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
-    function handleChange(e) {
-        const { name, value } = e.target;
-        let error = null;
-        switch(name) {
-            case "first_name":
-                if (value.length < 2) {
-                    error = "First Name must be at least 2 chars";
-                }
-            break;
-            case "last_name":
-                if (value.length < 3) {
-                    error = "Last Name must be at least 3 chars";
-                }
-            break;    
-            case "email":
-                if (value.length < 4) {
-                    error = "email must be at least 4 chars";
-                }
-            break;
-            default:
-                break;
-        }
-        dispatch({
-            type: name,
-            payload: { value: value, error: error }
-        });
-    }
     return (
         <form>
             <div>
                 <label>First Name</label>
-                <input type="text" name="first_name" id="first_name" onChange={handleChange} />
+                <input type="text" name="first_name" id="first_name" onChange={e => dispatch({type: 'first_name', payload: e.target.value })} />
                 {
                     state.first_name.error != null 
                     ?
@@ -65,7 +58,7 @@ const MyForm = () => {
             </div>
             <div>
                 <label>Last Name</label>
-                <input type="text" name="last_name" id="last_name" onChange={handleChange}/>
+                <input type="text" name="last_name" id="last_name" onChange={e => dispatch({type: 'last_name', payload: e.target.value })}/>
                 {
                     state.last_name.error != null 
                     ?
@@ -76,7 +69,7 @@ const MyForm = () => {
             </div>
             <div>
                 <label>Email</label>
-                <input type="text" name="email" id="email" onChange={handleChange}/>
+                <input type="text" name="email" id="email" onChange={e => dispatch({type: 'email', payload: e.target.value })}/>
                 {
                     state.email.error != null 
                     ?
