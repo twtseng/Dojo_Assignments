cf0a143ad0d56ce7f7158fff977461c111e4b6e6
twtseng <twtseng@users.noreply.github.com>
2020-09-29 10:38:44 -0700

    hw

---
+++ C#/Puzzles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Puzzles
+{
+    class Program
+    {
+        static void RandomArray()
+        {
+            List<int> arr = new List<int>();
+            Random rand = new Random();
+            for(int i=0; i < 10; ++i) 
+            {
+                int val = rand.Next(20) + 5;
+                arr.Add(val);
+            }
+            Console.WriteLine($"Min: {arr.Min()} Max: {arr.Max()} Avg: {arr.Average()} Arr: {string.Join(",", arr)}");
+        }
+        static void CoinFlip()
+        {
+            List<int> arr = new List<int>();
+            Random rand = new Random();
+            if (rand.Next(10) > 5)
+            {
+                Console.WriteLine("Heads");
+            }
+            else
+            {
+                Console.WriteLine("Tails");
+            }
+        }
+        static void Main(string[] args)
+        {
+            RandomArray();
+            foreach(var i in Enumerable.Range(1,20))
+            {
+                CoinFlip();
+            }
+        }
+    }
+}
