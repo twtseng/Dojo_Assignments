7fcfeaab46d3ad21df7dc19494fdacbed6c0bd25
twtseng <twtseng@users.noreply.github.com>
2020-09-29 10:01:52 -0700

    hw

---
+++ C#/Basic13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basic13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> oneTo255 = new List<int>();
+            List<int> odds = new List<int>();
+            for(int i=1; i <= 255; ++i) 
+            {
+                oneTo255.Add(i);
+                if (i % 2 == 1)
+                {
+                    odds.Add(i);
+                }
+            }
+            Console.WriteLine($"oneTo255: {string.Join(",", oneTo255)}");
+            Console.WriteLine($"odds: {string.Join(",", odds)}");
+        }
+    }
+}
