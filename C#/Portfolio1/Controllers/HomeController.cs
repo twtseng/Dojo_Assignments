14a6748abc1dc46e2e3bfbe78632af7523fb6612
twtseng <twtseng@users.noreply.github.com>
2020-10-02 13:47:20 -0700

    hw

---
+++ C#/Portfolio1/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Portfolio1.Models;
+
+namespace Portfolio1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet("")]
+        [HttpGet("foobar")]
+        public string Index()
+        {
+            return "This is my index";
+        }
+
+        [HttpGet("projects")]
+        public string Projects()
+        {
+            return "These are my projects";
+        }
+
+        [HttpGet("contact")]
+        public string Contact()
+        {
+            return "This is my contact";
+        }
+
+    }
+}
---
+++ C#/Portfolio1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Portfolio1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
