f4c3ab4afc69c1ff8fe43c840ae63943687f2678
twtseng <twtseng@users.noreply.github.com>
2020-10-02 15:05:55 -0700

    hw

---
+++ C#/DojoSurvey/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Survey()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SubmittedForm(string email, string password, bool checkvalue)
+        {
+            _logger.LogInformation($"email:{email} password:{password} checkvalue:{checkvalue}");
+            ViewBag.Email = email;
+            ViewBag.Password = password;
+            ViewBag.CheckValue = checkvalue;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
