0e643bdc3cafaa325a0022fb73e683e22488ef18
twtseng <twtseng@users.noreply.github.com>
2020-10-03 15:29:58 -0700

    hw

---
+++ C#/DojoSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet("survey")]
+        public IActionResult Survey()
+        {
+            return View();
+        }
+        [HttpPost("submittedsurvey")]
+        public IActionResult SubmittedSurvey(Survey survey)
+        {
+            return this.View(model: survey);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ C#/DojoSurveyWithModel/Models/Survey.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DojoSurveyWithModel.Models
+{
+    public class Survey
+    {
+        public string Name { get; set; }
+        public string  DojoLocation { get; set; }
+        public string FavoriteLanguage { get; set; }
+        public string Comment { get; set; }
+    }
+}
