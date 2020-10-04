16fb31672cc0ac12dab1fb40c3524489d54c1835
twtseng <twtseng@users.noreply.github.com>
2020-10-04 10:06:25 -0700

    hw

---
+++ C#/RandomPasscode/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RandomPasscode.Models;
+using Microsoft.AspNetCore.Http;
+
+
+namespace RandomPasscode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost("")]
+        public IActionResult GetNewPasscode()
+        {
+            const string CurrentPasscodeNumberKey="CurrentPasscodeNumber";
+            int? currentPasscodeNumber = HttpContext.Session.GetInt32(CurrentPasscodeNumberKey);
+            currentPasscodeNumber = currentPasscodeNumber.HasValue ? currentPasscodeNumber.Value+1 : 1;
+            HttpContext.Session.SetInt32(CurrentPasscodeNumberKey,currentPasscodeNumber.Value);
+            ViewBag.CurrentPasscodeNumber = currentPasscodeNumber.Value;
+            ViewBag.CurrentPasscode = System.Guid.NewGuid().ToString();
+            return View("Index");
+        }
+        [HttpPost("clearsession")]
+        public IActionResult ClearSession()
+        {
+            HttpContext.Session.Clear();
+            ViewBag.CurrentPasscodeNumber = 0;
+            ViewBag.CurrentPasscode = "";
+            return View("Index");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
